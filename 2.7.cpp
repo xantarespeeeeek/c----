d5bbcf2049073bc07b6de5782994a5a60fc74a39
xantarespeeeeek <824734441@qq.com>
2025-07-01 03:51:41 +0800

    ex2.7

---
+++ 2.7.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void display(int,int);
+int main()
+{
+    cout <<"Enter the number of hours:";
+    int hour;
+    cin >> hour;
+
+    cout <<"Enter the number of minutes:";
+    int min;
+    cin >> min;
+    
+    display(hour,min);
+    return 0;
+}
+
+void display(int h,int min)
+{
+    cout << "Time: " << h << ":" << min << endl;
+}
